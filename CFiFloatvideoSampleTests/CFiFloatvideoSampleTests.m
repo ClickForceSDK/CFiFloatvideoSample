56c7cdf5acf15181967a30e358fc417a7bf6574e
Eric <>
2019-07-05 14:19:14 +0800

    ClickForce IOS FloatVideo Sample Project Update

---
+++ CFiFloatvideoSampleTests/CFiFloatvideoSampleTests.m
@@ -0,0 +1,37 @@
+//
+//  CFiFloatvideoSampleTests.m
+//  CFiFloatvideoSampleTests
+//
+//  Created by CF-NB on 2019/7/4.
+//  Copyright © 2019年 CF-NB. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface CFiFloatvideoSampleTests : XCTestCase
+
+@end
+
+@implementation CFiFloatvideoSampleTests
+
+- (void)setUp {
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
